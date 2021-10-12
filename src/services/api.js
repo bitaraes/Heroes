0bc75929b8654c20385fabbb835a24c05d445f3e
Igor <igor_oliveira02@hotmail.com>
2021-10-11 22:35:31 -0300

    reqs exceptions

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,6 @@ const BASE_URL = "http://homologacao3.azapfy.com.br/api/ps/metahumans";
 export async function findHeroes() {
 	const req = await fetch(BASE_URL);
 	const json = await req.json();
+	if (req.status !== 200) throw new Error(json.error);
 	return json;
 }
